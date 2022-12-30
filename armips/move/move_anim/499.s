a0b2f0b762d1cd083154b6b54b10d4c5c653467d
Dray <drayhax@gmail.com>
2022-12-30 15:44:36 +0000

    Dual Chop, Sacred Sword, Razor Shell, Cotton Guard

--- armips/move/move_anim/499.s
+++ armips/move/move_anim/499.s
@@ -9,27 +9,33 @@
 
 .create "build/move/move_anim/0_499", 0
 
+// Dual Chop
+
 a010_499:
-    loadparticlefromspa 0, 486
-    waitparticle
-
-    addparticle 0, 2, 3
-    addparticle 0, 1, 3
-    addparticle 0, 1, 3
-    addparticle 0, 0, 3
-    wait 10
-
-    addparticle 0, 1, 3
-    addparticle 0, 1, 3
-    wait 10
-
-    addparticle 0, 2, 3
-    addparticle 0, 0, 3
-    waitparticle
-
-    unloadparticle 0
+    initspriteresource
+    loadspriteresource 0
+    loadspriteresource 1
+    loadspriteresource 2
+    loadspriteresource 3
+    loadspritemaybe 4, 0, 0, 0
+    loadspritemaybe 5, 0, 1, 1
+    loadspritemaybe 6, 0, 2, 2
+    loadspritemaybe 7, 0, 3, 3
+    callfunction 78, 1, 0, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    loadparticle 0, 46
     waitstate
+    unloadspriteresource
+    resetsprite 0
+    resetsprite 1
+    resetsprite 2
+    resetsprite 3
+    addparticle 0, 1, 4
+    addparticle 0, 0, 4
+    playsepan 2119, 117
+    waitse 1920, 117, 5
+    callfunction 36, 5, 3, 0, 1, 2, 264, "NaN", "NaN", "NaN", "NaN", "NaN"
+    waitparticle
+    unloadparticle 0
     end
-    
 
 .close

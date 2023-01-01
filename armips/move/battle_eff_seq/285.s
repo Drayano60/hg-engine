cb536367aa1dd9e505788af3cfb8fd751eeb2d2e
Dray <drayhax@gmail.com>
2023-01-01 17:34:41 +0000

    Add extra annotation

--- armips/move/battle_eff_seq/285.s
+++ armips/move/battle_eff_seq/285.s
@@ -10,7 +10,7 @@
 .create "build/move/battle_eff_seq/0_285", 0
 
 // +1 Speed effect
-// Redundant with eff_seq_12?
+// Used for Flame Charge and Trailblaze
 
 a030_285:
     changevar VAR_OP_SET, VAR_ADD_STATUS2, 0x11 | ADD_STATUS_ATTACKER

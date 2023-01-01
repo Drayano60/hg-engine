1ce191a365cdf357f1d3a650b3da233c5f1be32f
Dray <drayhax@gmail.com>
2023-01-01 17:43:31 +0000

    Fix shell smash order (defenses drop first)

--- armips/move/battle_sub_seq/318.s
+++ armips/move/battle_sub_seq/318.s
@@ -14,12 +14,16 @@
 a001_304:
     //attack
     ifmonstat IF_NOTEQUAL, BATTLER_ADDL_EFFECT, MON_DATA_STAT_STAGE_ATTACK, 12, StatusEffect
+    
     //sp atk
     ifmonstat IF_NOTEQUAL, BATTLER_ADDL_EFFECT, MON_DATA_STAT_STAGE_SPATK, 12, StatusEffect
+
     //def
     ifmonstat IF_NOTEQUAL, BATTLER_ADDL_EFFECT, MON_DATA_STAT_STAGE_DEFENSE, 0, StatusEffect
+
     //sp def
     ifmonstat IF_NOTEQUAL, BATTLER_ADDL_EFFECT, MON_DATA_STAT_STAGE_SPDEF, 0, StatusEffect
+
     //spe
     ifmonstat IF_EQUAL, BATTLER_ADDL_EFFECT, MON_DATA_STAT_STAGE_SPEED, 12, NoStatusEffect
 
@@ -28,21 +32,27 @@ StatusEffect:
     changevar VAR_OP_SETMASK, VAR_06, 0x200000
     changevar VAR_OP_SETMASK, VAR_06, 0x4001
     changevar VAR_OP_SETMASK, VAR_60, 0x80
-    //atk up
-    changevar VAR_OP_SET, VAR_34, 0x27
-    gotosubscript 12
-    //spa atk up
-    changevar VAR_OP_SET, VAR_34, 0x2A
-    gotosubscript 12
-    //spe up
-    changevar VAR_OP_SET, VAR_34, 0x29
-    gotosubscript 12
+
     //def down
     changevar VAR_OP_SET, VAR_34, DEFENSE_DOWN
     gotosubscript 12
+
     //sp def down
     changevar VAR_OP_SET, VAR_34, SPDEF_DOWN
     gotosubscript 12
+
+    //atk up
+    changevar VAR_OP_SET, VAR_34, 0x27
+    gotosubscript 12
+
+    //spa atk up
+    changevar VAR_OP_SET, VAR_34, 0x2A
+    gotosubscript 12
+
+    //spe up
+    changevar VAR_OP_SET, VAR_34, 0x29
+    gotosubscript 12
+
     changevar VAR_OP_CLEARMASK, VAR_60, 0x2
     changevar VAR_OP_CLEARMASK, VAR_60, 0x80
     endscript

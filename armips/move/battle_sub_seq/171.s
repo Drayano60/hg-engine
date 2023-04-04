e8fb5c6d4c1e314fbb7d7b47bf0f27a52428cdb4
Dray <drayhax@gmail.com>
2023-04-04 12:50:23 +0100

    Annotate Defog sub_seq

--- armips/move/battle_sub_seq/171.s
+++ armips/move/battle_sub_seq/171.s
@@ -9,6 +9,8 @@
 
 .create "build/move/battle_sub_seq/1_171", 0
 
+// Defog
+
 a001_171:
     checksidecondition BATTLER_DEFENDER, 0x1, 0x0, _00A8
     checksidecondition BATTLER_DEFENDER, 0x1, 0x1, _00A8
@@ -24,42 +26,50 @@ _00A8:
 _00B0:
     changevar VAR_OP_SET, VAR_34, 0x1C
     gotosubscript 12
+    /* Reflect */
     checksidecondition BATTLER_DEFENDER, 0x0, 0x0, _0108
     checksidecondition BATTLER_DEFENDER, 0x2, 0x0, _0108
     changevar VAR_OP_SET, VAR_MOVE_TEMP2, 0x73
     gotosubscript 172
 _0108:
+    /* Light Screen */
     checksidecondition BATTLER_DEFENDER, 0x0, 0x1, _0148
     checksidecondition BATTLER_DEFENDER, 0x2, 0x1, _0148
     changevar VAR_OP_SET, VAR_MOVE_TEMP2, 0x71
     gotosubscript 172
 _0148:
+    /* Mist */
     checksidecondition BATTLER_DEFENDER, 0x0, 0x2, _0188
     checksidecondition BATTLER_DEFENDER, 0x2, 0x2, _0188
     changevar VAR_OP_SET, VAR_MOVE_TEMP2, 0x36
     gotosubscript 172
 _0188:
+    /* Safeguard */
     checksidecondition BATTLER_DEFENDER, 0x0, 0x3, _01C8
     checksidecondition BATTLER_DEFENDER, 0x2, 0x3, _01C8
     changevar VAR_OP_SET, VAR_MOVE_TEMP2, 0xDB
     gotosubscript 172
 _01C8:
+    /* Spikes */
     checksidecondition BATTLER_DEFENDER, 0x0, 0x4, _0218
     checksidecondition BATTLER_DEFENDER, 0x2, 0x4, _0218
     changevar VAR_OP_CLEARMASK, VAR_SIDE_EFFECT_OPPONENT, 0x4
     changevar VAR_OP_SET, VAR_MOVE_TEMP2, 0xBF
     gotosubscript 172
 _0218:
+    /* Toxic Spikes */
     checksidecondition BATTLER_DEFENDER, 0x0, 0x5, _0258
     checksidecondition BATTLER_DEFENDER, 0x2, 0x5, _0258
     changevar VAR_OP_SET, VAR_MOVE_TEMP2, 0x186
     gotosubscript 172
 _0258:
+    /* Stealth Rock */
     if IF_NOTMASK, VAR_SIDE_EFFECT_OPPONENT, 0x80, _0294
     changevar VAR_OP_CLEARMASK, VAR_SIDE_EFFECT_OPPONENT, 0x80
     changevar VAR_OP_SET, VAR_MOVE_TEMP2, 0x1BE
     gotosubscript 172
 _0294:
+    /* Foggy Weather */
     if IF_NOTMASK, VAR_FIELD_EFFECT, 0x8000, _02D8
     changevar VAR_OP_CLEARMASK, VAR_FIELD_EFFECT, 0x8000
     printmessage 0x415, 0xA, 0x1, 0x1, "NaN", "NaN", "NaN", "NaN"

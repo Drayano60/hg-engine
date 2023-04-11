b8d4040c69816e2c2f28e211bad92288d1e33a3e
Dray <drayhax@gmail.com>
2023-04-11 19:57:00 +0100

    Make message show up after an Air Lock or Cloud Nine Pokemon comes in

---
+++ armips/move/battle_sub_seq/347.s
@@ -0,0 +1,16 @@
+.nds
+.thumb
+
+.include "armips/include/battlescriptcmd.s"
+
+.create "build/move/battle_sub_seq/1_347", 0x0
+
+/* air lock message printing battle script */
+
+a001_347:
+    printmessage 1383, 0x0, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    waitmessage
+    wait 0x1E
+    endscript
+
+.close
--- src/battle/ability.c
+++ src/battle/ability.c
@@ -288,6 +288,7 @@ enum
     SWITCH_IN_CHECK_FAIRY_AURA,
     SWITCH_IN_CHECK_AURA_BREAK,
     SWITCH_IN_CHECK_IMPOSTER,
+    SWITCH_IN_CHECK_AIR_LOCK,
     SWITCH_IN_CHECK_END,
 };
 
@@ -1077,6 +1078,30 @@ int SwitchInAbilityCheck(void *bw, struct BattleStruct *sp)
                         sp->battlemon[sp->attack_client].pp[i] = 5;
                     }
                 }
+                break;
+            case SWITCH_IN_CHECK_AIR_LOCK:
+                for (i = 0; i < client_set_max; i++)
+                {
+                    client_no = sp->turn_order[i];
+                    if ((sp->battlemon[client_no].air_lock_flag == 0)
+                        && (sp->battlemon[client_no].hp)
+                        && ((GetBattlerAbility(sp, client_no) == ABILITY_CLOUD_NINE) ||
+                            (GetBattlerAbility(sp, client_no) == ABILITY_AIR_LOCK)))
+                    {
+                        sp->battlemon[client_no].air_lock_flag = 1;
+                        sp->client_work = client_no;
+
+                        scriptnum = SUB_SEQ_HANDLE_AIR_LOCK_MESSAGE;
+
+                        ret = SWITCH_IN_CHECK_MOVE_SCRIPT;
+                        break;
+                    }
+                }
+                if (i == client_set_max)
+                {
+                    sp->switch_in_check_seq_no++;
+                }
+
                 break;
                 // 02253D78
             case SWITCH_IN_CHECK_END:

50379e7cde1664039ccd27dea06b42c81d842a4e
Dray <drayhax@gmail.com>
2023-04-11 21:59:15 +0100

    Change Air Lock message to make it more clear

--- armips/move/battle_sub_seq/347.s
+++ armips/move/battle_sub_seq/347.s
@@ -8,7 +8,7 @@
 /* air lock message printing battle script */
 
 a001_347:
-    printmessage 1383, 0x0, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    printmessage 1383, 0xB, 0xFF, 0x15, "NaN", "NaN", "NaN", "NaN"
     waitmessage
     wait 0x1E
     endscript

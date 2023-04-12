1feb5711364ad0228d771c00ea89e03b5253836e
Dray <drayhax@gmail.com>
2023-04-12 18:55:06 +0100

    Make Life Orb display a message after the health drain

--- armips/move/battle_sub_seq/214.s
+++ armips/move/battle_sub_seq/214.s
@@ -11,7 +11,25 @@
 
 a001_214:
     changevar VAR_OP_SETMASK, VAR_06, 0x40
-    gotosubscript 2
+_Subscript2:
+    if IF_MASK, VAR_06, 0x40, _0044
+    playmovesoundeffect BATTLER_xFF
+    monflicker 0xFF
+    waitmessage
+    if IF_EQUAL, VAR_69, 0x0, _0044
+    gotosubscript 264
+_0044:
+    changevar VAR_OP_CLEARMASK, VAR_06, 0x40
+    healthbarupdate BATTLER_xFF
+    waitmessage
+    printmessage 1389, 0x2, 0x1, "NaN", "NaN", "NaN", "NaN", "NaN"
+    waitmessage
+    wait 0x1E
+    datahpupdate BATTLER_xFF
+    tryfaintmon BATTLER_xFF
+    if IF_GREATER, VAR_HP_TEMP, 0x0, _0094
+    changevar2 VAR_OP_SET, VAR_ASSURANCE_DAMAGE, VAR_HP_TEMP
+_0094:
     endscript
 
 .close

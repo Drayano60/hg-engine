01a931dceac7cdf327e6ae7aa9f1575c81084844
Dray <drayhax@gmail.com>
2023-04-30 15:29:50 +0100

    Update Skitter Smack animation

--- armips/move/move_anim/540.s
+++ armips/move/move_anim/540.s
@@ -10,7 +10,7 @@
 .create "build/move/move_anim/0_540", 0
 
 // Skitter Smack
-// Quick Attack animation
+// Copied from my Acrobatics with some slight edits
 
 a010_540:
     initspriteresource
@@ -23,9 +23,12 @@ a010_540:
     loadspritemaybe 6, 0, 2, 2
     loadspritemaybe 7, 0, 3, 3
     callfunction 78, 1, 0, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
-    loadparticle 0, 128
+    loadparticle 1, 32
     waitstate
     unloadspriteresource
+
+    callfunction 33, 5, 0, 1, 0, 12, 0, "NaN", "NaN", "NaN", "NaN", "NaN"
+
     resetsprite 0
     resetsprite 1
     resetsprite 2
@@ -33,26 +36,61 @@ a010_540:
     initspriteresource
     loadspriteresource 0
     loadspriteresource 1
-    playsepan 1983, -117
-    loadspritemaybe 0, 0, 0, 0
-    loadspritemaybe 0, 0, 1, 1
-    loadspriteresource 4
-    loadspritemaybe 2, 0, 4, 4
-    cmd52 2, 0, 4
-    callfunction 8, 0, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
-    wait 15
-    playsepan 1827, 117
+    loadspriteresource 2
+    loadspriteresource 3
+    loadspritemaybe 4, 0, 0, 0
+    loadspritemaybe 5, 0, 1, 1
+    loadspritemaybe 6, 0, 2, 2
+    loadspritemaybe 7, 0, 3, 3
+    callfunction 78, 1, 0, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    loadparticle 0, 263
+    waitstate
+    unloadspriteresource
     resetsprite 0
     resetsprite 1
-    unloadspriteresource
-    cmd53 0
-    resetsprite 4
-    addparticle 0, 1, 4
-    addparticle 0, 0, 4
-    callfunction 36, 5, 2, 0, 1, 1, 264, "NaN", "NaN", "NaN", "NaN", "NaN"
+    resetsprite 2
+    resetsprite 3
+    cmd0C 0, -32
+    cmd0C 1, 0
+    cmd0C 6, 1
+    cmd0C 7, 1
+    // addparticle 0, 1, 4
+    wait 1
+    callfunction 50, 2, 5, 0, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    playsepan 1911, -117
+    callfunction 15, 0, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    // addparticle 0, 0, 3 // smoke cloud
     waitstate
+    // addparticle 0, 1, 4
+
+    // Whack
+    playsepan 1827, 117
+    addparticle 1, 1, 4
+    callfunction 36, 5, 1, 0, 1, 2, 264, "NaN", "NaN", "NaN", "NaN", "NaN"
+    wait 8
+    playsepan 1827, 117
+    addparticle 1, 1, 4
+    callfunction 36, 5, 1, 0, 1, 2, 264, "NaN", "NaN", "NaN", "NaN", "NaN"
+    wait 8
+    playsepan 1827, 117
+    addparticle 1, 1, 4
+    callfunction 36, 5, 1, 0, 1, 2, 264, "NaN", "NaN", "NaN", "NaN", "NaN"
+    wait 8
+
+    waitstate
+    repeatse 1983, -117, 2, 2
+    callfunction 50, 2, 5, 0, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    waitstate
+    freeresources 0
     waitparticle
     unloadparticle 0
+    cmd0C 0, -32
+    cmd0C 1, 0
+    cmd0C 6, 1
+    cmd0C 7, 1
+
+    callfunction 33, 5, 0, 1, 12, 0, 0, "NaN", "NaN", "NaN", "NaN", "NaN"
+
     end
 
 

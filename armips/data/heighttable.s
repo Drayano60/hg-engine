b37e4e1d25e65a7dbfa524394d02d6214d4da13d
Dray <drayhax@gmail.com>
2023-11-12 23:46:38 +0000

    Adjust a couple sprites

--- armips/data/heighttable.s
+++ armips/data/heighttable.s
@@ -761,7 +761,7 @@ heightentry SPECIES_TYRUNT, 22, 22, 12, 12
 heightentry SPECIES_TYRANTRUM, 0, 0, 0, 0
 heightentry SPECIES_AMAURA, 8, 8, 0, 0
 heightentry SPECIES_AURORUS, 0, 0, 0, 0
-heightentry SPECIES_SYLVEON, 0, 0, 0, 0
+heightentry SPECIES_SYLVEON, 3, 3, 0, 0
 heightentry SPECIES_HAWLUCHA, 0, 0, 6, 6
 heightentry SPECIES_DEDENNE, 0, 0, 16, 16
 heightentry SPECIES_CARBINK, 7, 7, 0, 0

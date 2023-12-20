e093d8497da33319fd624ebbd82b412bac46dc84
Dray <drayhax@gmail.com>
2023-12-20 01:56:53 +0000

    Rebuild text narc every time

--- narcs.mk
+++ narcs.mk
@@ -611,10 +611,10 @@ $(SCR_SEQ_NARC): $(SCR_SEQ_DEPENDENCIES)
 	for file in $^; do $(ARMIPS) $$file; done
 	$(NARCHIVE) create $@ $(SCR_SEQ_DIR) -nf
 
-# for convenience, rebuild SCR_SEQ_NARC every build so that DSPRE changes are not overwritten
+# for convenience, rebuild these narcs every build so that DSPRE changes are not overwritten
 .PHONY: $(SCR_SEQ_NARC)
 .PHONY: $(DEXSORT_NARC)
-
+.PHONY: $(MSGDATA_NARC)
 
 NARC_FILES += $(SCR_SEQ_NARC)
 

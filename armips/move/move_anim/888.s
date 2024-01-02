8f3b71deebf3609218f67d8e79e770052eac2575
Dray <drayhax@gmail.com>
2024-01-02 19:01:21 +0000

    Redo Trailblaze animation cause the old one crashed the game sometimes

--- armips/move/move_anim/888.s
+++ armips/move/move_anim/888.s
@@ -10,7 +10,8 @@
 .create "build/move/move_anim/0_888", 0
 
 // Trailblaze
-// Quick Attack animation with some Leaf Blade leaves
+// Side-to-side transition with first QA sound effect repeated twice,
+// followed by Tackle animation with target highlighted green + Grass Knot leaves and sound effect.
 
 a010_888:
     initspriteresource
@@ -23,39 +24,44 @@ a010_888:
     loadspritemaybe 6, 0, 2, 2
     loadspritemaybe 7, 0, 3, 3
     callfunction 78, 1, 0, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
-    loadparticle 0, 128
+    loadparticle 0, 64
+    loadparticle 1, 465
     waitstate
     unloadspriteresource
     resetsprite 0
     resetsprite 1
     resetsprite 2
     resetsprite 3
-    initspriteresource
-    loadspriteresource 0
-    loadspriteresource 1
+
     playsepan 1983, -117
-    loadspritemaybe 0, 0, 0, 0
-    loadspritemaybe 0, 0, 1, 1
-    loadspriteresource 4
-    loadspritemaybe 2, 0, 4, 4
-    cmd52 2, 0, 4
-    callfunction 8, 0, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    callfunction 52, 3, 8, -12, 258, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    waitstate
+    callfunction 52, 3, 8, 24, 258, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    waitstate
+    playsepan 1983, -117
+    callfunction 52, 3, 8, -24, 258, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    waitstate
+    callfunction 52, 3, 8, 24, 258, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    waitstate
+    callfunction 52, 3, 8, -12, 258, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    waitstate
 
-
-    // leaves
-    loadparticle 0, 366
+    playsepan 1833, 117
+    callfunction 57, 4, 2, 14, -8, 258, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    waitstate
+    addparticle 0, 1, 4
     addparticle 0, 0, 4
+    callfunction 34, 6, 8, 0, 1, 11252, 10, 25, "NaN", "NaN", "NaN", "NaN"
+    callfunction 36, 5, 1, 0, 1, 2, 264, "NaN", "NaN", "NaN", "NaN", "NaN"
+    callfunction 57, 4, 2, -14, 8, 258, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
 
-    wait 20
-    playsepan 1827, 117
-    cmd53 0
-    resetsprite 4
+    addparticle 1, 1, 4
+    playsepan 1897, 117
 
-
-    callfunction 36, 5, 2, 0, 1, 1, 264, "NaN", "NaN", "NaN", "NaN", "NaN"
     waitstate
     waitparticle
     unloadparticle 0
+    unloadparticle 1
     end
 
 .close

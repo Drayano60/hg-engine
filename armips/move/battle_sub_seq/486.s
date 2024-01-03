69f022eeb304c6b37d76e45535f3f15973bd03da
Dray <drayhax@gmail.com>
2024-01-03 01:50:38 +0000

    Fix issue with Mental Herb resetting move selection when curing Disable

--- armips/move/battle_sub_seq/486.s
+++ armips/move/battle_sub_seq/486.s
@@ -10,15 +10,9 @@
 .create "build/move/battle_sub_seq/1_486", 0
 
 /** An attempt at Mental Herb stuff
-
-This is called when any of Taunt, Torment, Encore, Disable and Heal Block are initially applied.
-It just cures all of the relevant statuses when activated.
-
+This is called when any of Taunt, Torment, Encore, Disable and Heal Block are initially applied, and cures the effect.
 This won't be called if a Pokémon gains a Mental Herb while already under the effects of these statuses.
-
 */
-
-
 
 a001_486:
     ifmonstat IF_NOTEQUAL, BATTLER_DEFENDER, MON_DATA_ITEM, ITEM_MENTAL_HERB, _End
@@ -27,26 +21,32 @@ a001_486:
     printmessage 1506, TAG_NICK_ITEM, BATTLER_DEFENDER, BATTLER_DEFENDER, "NaN", "NaN", "NaN", "NaN"
     waitmessage
     wait 0x1E
-
-    // Cure Taunt
+    if IF_EQUAL, VAR_CURRENT_MOVE, MOVE_TAUNT, Taunt
+    if IF_EQUAL, VAR_CURRENT_MOVE, MOVE_TORMENT, Torment
+    if IF_EQUAL, VAR_CURRENT_MOVE, MOVE_ENCORE, Encore
+    if IF_EQUAL, VAR_CURRENT_MOVE, MOVE_DISABLE, Disable
+    if IF_EQUAL, VAR_CURRENT_MOVE, MOVE_HEAL_BLOCK, HealBlock
+    goto _End
+Taunt:
     changemondatabyvar VAR_OP_SET, BATTLER_DEFENDER, MON_DATA_TAUNT_COUNTER, 0
-    
-    // Cure Torment
-    changemondatabyvalue VAR_OP_CLEARMASK, BATTLER_DEFENDER, MON_DATA_STATUS_2, 0x80000000 // Torment
-    
-    // Cure Encore
+    goto RemoveItem
+Torment:
+    changemondatabyvalue VAR_OP_CLEARMASK, BATTLER_DEFENDER, MON_DATA_STATUS_2, 0x80000000
+    goto RemoveItem
+Encore:
     // Should this also clear encored move index? Seems to work OK without though.
-    changemondatabyvar VAR_OP_SET, BATTLER_DEFENDER, MON_DATA_ENCORE_COUNTER, 0 // Encore
-    changemondatabyvar VAR_OP_SET, BATTLER_DEFENDER, MON_DATA_ENCORED_MOVE, 0 // Encore
-
-    // Cure Disable
+    changemondatabyvar VAR_OP_SET, BATTLER_DEFENDER, MON_DATA_ENCORE_COUNTER, 0
+    changemondatabyvar VAR_OP_SET, BATTLER_DEFENDER, MON_DATA_ENCORED_MOVE, 0
+    goto RemoveItem
+Disable:
     changemondatabyvar VAR_OP_SET, BATTLER_DEFENDER, MON_DATA_DISABLED_MOVE, 0
     changemondatabyvar VAR_OP_SET, BATTLER_DEFENDER, MON_DATA_DISABLE_COUNTER, 0
-
-    // Cure Heal Block
-    changemondatabyvalue VAR_OP_SETMASK, BATTLER_DEFENDER, MON_DATA_MOVE_STATE, 0x2000000
+    goto RemoveItem
+HealBlock:
+    changemondatabyvalue VAR_OP_CLEARMASK, BATTLER_DEFENDER, MON_DATA_MOVE_STATE, 0x2000000 // What does this do?
     changemondatabyvar VAR_OP_SET, BATTLER_DEFENDER, MON_DATA_HEAL_BLOCK_COUNTER, 0
-
+    goto RemoveItem
+RemoveItem:
     removeitem BATTLER_DEFENDER
 _End:
     endscript

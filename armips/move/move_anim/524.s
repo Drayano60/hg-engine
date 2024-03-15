210fc8c9c148d09db444acfd1af215b73707eb6b
Dray <drayhax@gmail.com>
2024-03-15 20:14:08 +0000

    Fix Volt Switch crash

--- armips/move/move_anim/524.s
+++ armips/move/move_anim/524.s
@@ -40,6 +40,10 @@ _00C8:
     resetsprite 1
     resetsprite 2
     resetsprite 3
+
+    callfunction 33, 5, 0, 1, 0, 12, 0, "NaN", "NaN", "NaN", "NaN", "NaN"
+    waitstate
+
     wait 5
     repeatse 1971, 0, 2, 3
     addparticle 0, 0, 17
@@ -56,8 +60,10 @@ _00C8:
     waitstate
     waitparticle
     unloadparticle 0
-    unloadparticle 1
-    unloadparticle 2
+
+    callfunction 33, 5, 0, 1, 12, 0, 0, "NaN", "NaN", "NaN", "NaN", "NaN"
+    waitstate
+    
     end
 _0268:
     addparticle 0, 0, 3
@@ -68,23 +74,41 @@ _0268:
     jumpifside 0, _02CC, _02EC
     waitparticle
     unloadparticle 0
-    unloadparticle 1
-    unloadparticle 2
     end
 _02CC:
     addparticle 0, 6, 3
     waitparticle
     unloadparticle 0
-    unloadparticle 1
-    unloadparticle 2
     end
 _02EC:
     addparticle 0, 7, 3
     waitparticle
     unloadparticle 0
-    unloadparticle 1
-    unloadparticle 2
     end
-    
+_030C:
+    callfunction 40, 2, 2, 1, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    playsepan 1839, -117
+    wait 10
+    playsepan 1823, 0
+    addparticle 0, 1, 17
+    cmd37 6, 0, 2, 1, 1, 0, 0, "NaN", "NaN"
+    wait 8
+    playsepan 1827, 117
+    addparticle 0, 4, 17
+    cmd37 6, 0, 2, 2, 0, 0, 0, "NaN", "NaN"
+    addparticle 0, 2, 17
+    cmd37 6, 0, 2, 2, 0, 0, 0, "NaN", "NaN"
+    addparticle 0, 5, 17
+    cmd37 6, 0, 2, 2, 0, 0, 0, "NaN", "NaN"
+    callfunction 36, 5, 2, 0, 1, 2, 264, "NaN", "NaN", "NaN", "NaN", "NaN"
+    wait 15
+    playsepan 1839, -117
+    addparticle 0, 3, 3
+    wait 5
+    addparticle 0, 0, 3
+    callfunction 40, 2, 2, 0, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+    waitparticle
+    unloadparticle 0
+    end
 
 .close

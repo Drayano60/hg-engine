10f9301f036f24258978ae4b1a3cdb40400566dc
Dray <drayhax@gmail.com>
2024-03-16 18:59:28 +0000

    Revert "Change 2-hit moves to be double BP but halved damage for AI reasons"
    This reverts commit 10bf44cb65b6945ca6ce3005ebaa96ce66b4b6a9.

--- armips/move/battle_eff_seq/044.s
+++ armips/move/battle_eff_seq/044.s
@@ -12,11 +12,6 @@
 // Multi-strike moves that always hit twice
 
 a030_044:
-    /* The BP of these two-hit moves has been set to their full power,
-       but the damage is halved in the battle effect here.
-       This is so the AI reads the moves at their full power. */
-    changevar VAR_OP_SET, VAR_DAMAGE_MULT, 5
-
     setmultihit 0x2, 0xFD
     changevar VAR_OP_SET, VAR_SUCCESSIVE_HIT, 0x1
     gotosubscript 466

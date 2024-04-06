56aefeee8ae9f4478744ea10ccefe41db6779a7a
Dray <drayhax@gmail.com>
2024-04-06 11:50:30 +0100

    AI edit for Erika's team

--- armips/asm/custom/ai_edits.s
+++ armips/asm/custom/ai_edits.s
@@ -20,4 +20,12 @@
 .org 0x02224488
 .halfword MOVE_NONE
 
+// Chlorophyll gives +1 to Sunny Day instead of Flower Gift
+.org 0x0221BE20 + 0xA184
+.halfword ABILITY_CHLOROPHYLL
+
+// Change the score increase from +1 to +3
+.org 0x0221BE20 + 0xA1B4
+.byte 03
+
 .close

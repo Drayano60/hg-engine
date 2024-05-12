48e7d4d072f0d8e4de39b4f322c7343c6507d6d3
Dray <drayhax@gmail.com>
2024-05-12 17:14:53 +0100

    config.s

--- armips/include/config.s
+++ armips/include/config.s
@@ -19,7 +19,7 @@ SNOW_WARNING_GENERATION equ 9
 .definelabel ALLOW_SAVE_CHANGES, 0
 
 // CRY_PSEUDOBANK_START defines the first pseudobank to be used as cries in the sdat.  cries are loaded differently to save on RAM space
-CRY_PSEUDOBANK_START equ 778
+CRY_PSEUDOBANK_START equ 788 /* Modified */
 
 // DEBUG_NEEDS_TESTING defines in progress implementations that may not necessarily function.
 DEBUG_NEEDS_TESTING equ 0
@@ -31,16 +31,16 @@ LEARNSET_TOTAL_MOVES equ 41 // 40+terminate - currently driven by gallade
 BATTLE_MODE_FORCE_SET equ 0
 
 // ALWAYS_HAVE_NATIONAL_DEX defines whether or not the player will always have the national dex unlocked.
-ALWAYS_HAVE_NATIONAL_DEX equ 0
+ALWAYS_HAVE_NATIONAL_DEX equ 1 /* Modified */
 
 // ALWAYS_UNCAPPED_FRAME_RATE defines whether or not the frame rate of the game will always be uncapped, including in the overworld.
 ALWAYS_UNCAPPED_FRAME_RATE equ 0
 
 // BATTLES_UNCAPPED_FRAME_RATE defines whether or not the frame rate of the game will be uncapped solely in battles or not.
-BATTLES_UNCAPPED_FRAME_RATE equ 0
+BATTLES_UNCAPPED_FRAME_RATE equ 1 /* Modified */
 
 // FAST_TEXT_PRINTING will make the text print quickly, almost instantly
-FAST_TEXT_PRINTING equ 0
+FAST_TEXT_PRINTING equ 1 /* Modified */
 
 // REUSABLE_TMS will make TMs infinite and hide the quantity number.
 REUSABLE_TMS equ 1

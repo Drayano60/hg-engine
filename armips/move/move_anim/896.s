b700ccc241b1bfe8b429d6b7099001dbc28dc348
Dray <drayhax@gmail.com>
2024-07-08 19:05:29 +0100

    Improve Gigaton Hammer animation

--- armips/move/move_anim/896.s
+++ armips/move/move_anim/896.s
@@ -11,8 +11,7 @@
 .create "build/move/move_anim/0_896", 0
 
 // Gigaton Hammer
-// Body Slam animation with extra squish
-// This should really be better
+// Focus Energy + Body Slam forward movement + Hammer Arm squish + Head Smash bg + Dragon Rush rock/dust particles
 
 a010_896:
     initspriteresource
@@ -25,27 +24,53 @@ a010_896:
     loadspritemaybe 6, 0, 2, 2
     loadspritemaybe 7, 0, 3, 3
     callfunction 78, 1, 0, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
-    loadparticle 0, 65
+    loadparticle 0, 425
+    loadparticle 1, 143
     waitstate
     unloadspriteresource
     resetsprite 0
     resetsprite 1
     resetsprite 2
     resetsprite 3
-    callfunction 57, 4, 4, 0, 16, 258, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
+
+    addparticle 1, 1, 3
+    addparticle 1, 0, 3
+    callfunction 36, 5, 1, 0, 1, 4, 258, "NaN", "NaN", "NaN", "NaN", "NaN"
+    callfunction 34, 6, 2, 0, 2, 32767, 10, 0, "NaN", "NaN", "NaN", "NaN"
+    playsepan 1965, -117
     waitstate
-    callfunction 57, 4, 4, 0, -16, 258, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
-    waitstate
-    playsepan 1927, 117
-    waitse 1920, 117, 6
+    waitparticle
+
     callfunction 57, 4, 4, 24, 0, 258, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
     waitstate
-    addparticle 0, 1, 4
-    addparticle 0, 0, 4
-    callfunction 36, 5, 3, 0, 1, 4, 264, "NaN", "NaN", "NaN", "NaN", "NaN"
-    callfunction 42, 8, 264, 100, 130, 100, 70, 100, 1310721, 262148, "NaN", "NaN"
+
+    cmd43
+    cmd0C 7, 1
+    changebg 40, 0x1
+
+    callfunction 68, 5, 8, 8, 0, 8, 0, "NaN", "NaN", "NaN", "NaN", "NaN"
+    callfunction 42, 8, 2056, 100, 100, 100, 60, 100, 1966081, 655370, "NaN", "NaN"
+
+    playsepan 1909, 117
+    addparticle 0, 3, 17
+    cmd37 6, 0, 2, 2, 0, 0, 0, "NaN", "NaN"
+    addparticle 0, 0, 17
+    cmd37 6, 0, 2, 2, 0, 0, 0, "NaN", "NaN"
+
+    playsepan 1954, 117
+    waitstate
+    waitparticle
+
     callfunction 57, 4, 4, -24, 0, 258, "NaN", "NaN", "NaN", "NaN", "NaN", "NaN"
     waitstate
+
+    cmd43
+    cmd0C 7, 1
+    resetbg 40, 0x1
+    waitforchangebg
+    
+    unloadparticle 0
+    unloadparticle 1
     end
     
 
